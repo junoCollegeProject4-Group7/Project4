2f5a54a199b70af46d6b4744daab5c654c82d069
Sammy <sammylam_1@hotmail.com>
2022-11-04 11:40:47 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -3,37 +3,27 @@ import './App.scss';
 
 import Generator from './pages/Generator';
 import About from './pages/About';
-import {useState, useEffect} from "react";
+import { useState, useEffect } from 'react';
 // this is used to access and store data from/to firebase
 import { getDatabase, ref, onValue } from 'firebase/database';
 import firebaseConfig from './firebase';
 
 function App() {
- const [players, setplayers] = useState([]);
+	const [players, setplayers] = useState([]);
 
-    useEffect(()=>{
-    //     // variable that holds data details
-        const database = getDatabase(firebaseConfig);
-    //     // references the database
-        const databaseref = ref(database);
-    //     // adding event listener to the variable from firebase 
-        onValue (databaseref, (response) => {
-            console.log(response.val())
-        })
-    }, [])
-    return (
-        <div>
-            <h1>this is a test</h1>
-            <ul>
-                {players.map((player)=>{
-                    <li>
-                        <p>{player}</p>
-                    </li>
-                })}
-            </ul>
-        </div>
-        <div className="wrapper">
-            <header>
+	useEffect(() => {
+		//     // variable that holds data details
+		const database = getDatabase(firebaseConfig);
+		//     // references the database
+		const databaseref = ref(database);
+		//     // adding event listener to the variable from firebase
+		onValue(databaseref, (response) => {
+			console.log(response.val());
+		});
+	}, []);
+	return (
+		<div className='wrapper'>
+			{/* <header>
                 <Link to="/">
                     <h1>Trivia Time</h1>
                 </Link>
@@ -43,11 +33,9 @@ function App() {
                 <Route path='./pages/Generator.js' element={<Generator />} />
                 {/* <Route path='./pages/Quiz.js' element={<Quiz />} />
                 <Route path='./pages/Scoreboard' element={<Scoreboard />} /> */}
-            </Routes>
-        </div>
-    )
+			{/* </Routes> */}
+		</div>
+	);
 }
-
-
 
 export default App;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
-import AboutModal from '../components/AboutModal'
+import AboutModal from '../components/AboutModal';
 
 const About = () => {
-  return (
-    <>
-    <h1>About Page</h1>
-    <AboutModal />
-    </>
-  )
-}
+	return (
+		<>
+			<h1>About Page</h1>
+			<AboutModal />
+		</>
+	);
+};
 
-export default About
+export default About;
