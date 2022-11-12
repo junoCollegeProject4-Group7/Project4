e2a9068cfca6cd6eaa2410eb4b34d871b80486e8
olsikule <olsi.kule@gmail.com>
2022-11-12 11:33:28 -0500

    updated timer.js and variable names on question and quiz components

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,80 +1,79 @@
-import React from 'react';
-import { useState } from 'react';
-import { getDatabase, ref, set, update } from 'firebase/database';
+import React from "react";
+import { useState } from "react";
+import { getDatabase, ref, set, update } from "firebase/database";
 
-import { useEffect } from 'react';
-import Timer from './Timer';
+import { useEffect } from "react";
 
 //api is https://opentdb.com/api_config.php
 
 //example api https://opentdb.com/api.php?amount=10&category=25&difficulty=medium&type=multiple
-const QuestionCard = ({ question, userName }) => {
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [score, setScore] = useState(0);
-	const [timerReset, setTimerReset] = useState(false);
+const QuestionCard = ({ question, userName, resetTimer }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
 
-	const questions = question.map(function (q) {
-		return q.question;
-	});
+  const questions = question.map(function (q) {
+    return q.question;
+  });
 
-	const answerBank = question.map(function (ans) {
-		return ans.incorrect_answers + ',' + ans.correct_answer;
-	});
+  const answerBank = question.map(function (ans) {
+    return ans.incorrect_answers + "," + ans.correct_answer;
+  });
 
-	const correctAns = question.map(function (answer) {
-		return answer.correct_answer;
-	});
+  const correctAns = question.map(function (answer) {
+    return answer.correct_answer;
+  });
 
-	const updateFirebase = () => {
-		const db = getDatabase();
-		update(ref(db, `users/${userName}`), {
-			score: score,
-		});
-	};
+  const updateFirebase = () => {
+    const db = getDatabase();
+    update(ref(db, `users/${userName}`), {
+      score: score,
+    });
+  };
 
-	useEffect(() => {
-		updateFirebase();
-	}, [score]);
+  useEffect(() => {
+    updateFirebase();
+  }, [score]);
 
-	const handleSubmit = (e) => {
-		setCurrentQuestion(currentQuestion + 1);
-		if (e.target.textContent === correctAns[currentQuestion]) {
-			setScore(score + 1);
-		} else {
-			setScore(score);
-		}
+  const handleSubmit = (e) => {
+    resetTimer();
+    setCurrentQuestion(currentQuestion + 1);
+    if (e.target.textContent === correctAns[currentQuestion]) {
+      setScore(score + 1);
+    } else {
+      setScore(score);
+    }
 
-		if (currentQuestion === currentQuestion.length) {
-			//errorpage
-		}
-	};
+    if (currentQuestion === currentQuestion.length) {
+      //errorpage
+    }
+  };
 
-	return (
-		<>
-			<div className='card'>
-				<Timer currentQuestion={currentQuestion} />
-				<div className='question'>{questions[currentQuestion]}</div>
-				<div className='answers'>
-					<div>
-						{/* splits answer array by delimiter and maps the array adding a button to handle  */}
-						{answerBank[currentQuestion].split(',').map((ans) => {
-							return (
-								<button
-									onClick={(e) => {
-										handleSubmit(e);
-									}}
-								>
-									{ans}
-								</button>
-							);
-						})}
-					</div>
-				</div>
-				<div className='score-section'>
-					You scored {score} out of {questions.length}
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="card">
+        <div className="question">{questions[currentQuestion]}</div>
+        <div className="answers">
+          <div>
+            {/* splits answer array by delimiter and maps the array adding a button to handle  */}
+            {answerBank[currentQuestion].split(",").map((ans) => {
+              return (
+                <button
+                  key={ans}
+                  onClick={(e) => {
+                    handleSubmit(e);
+                  }}
+                >
+                  {ans}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      </div>
+    </>
+  );
 };
 export default QuestionCard;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,37 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
 
-const Timer = (currentQuestion) => {
-	const [counter, setCounter] = useState(30);
-	//yolo solo 
-
-	useEffect(() => {
-		const countdown =
-		  counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-		return () => clearInterval(countdown);
-	}, [counter]);
-	
-	// countdown();
-	if (currentQuestion === currentQuestion + 1) {
-		countdown(30);
-	}
-
-	
-	// useEffect(() => {
-	// const countdown = () => {
-	// 	setCounter(30);
-	// 	const intervalID = setInterval(() => {
-	// 		setCounter((current) => (current - 1 < 0 ? 0 : current - 1));
-	// 	}, 1000);
-		
-	// 	console.log("caanter", setCounter)
-	// 	return () => {
-	// 		clearInterval(intervalID);
-	// 	} 
-	// };
-
-
-
-	return <span>{counter}</span>;
+const Timer = ({ count }) => {
+  return <span>{count}</span>;
 };
 
 export default Timer;
