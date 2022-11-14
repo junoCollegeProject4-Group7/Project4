6c8866252f7148f2b988c286986e01e30c19120e
PPableo <pableop@gmail.com>
2022-11-13 22:25:06 -0500

    Reverted

---
+++ src/pages/Error.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Error = () => {
+	return <div>Sorry This page doesn't exist</div>;
+};
+
+export default Error;
--- src/pages/Scoreboard.js
+++ src/pages/Scoreboard.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { getDatabase, ref, onValue, push } from 'firebase/database';
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { useState, useEffect } from 'react';
+import firebaseConfig from '../firebase';
 
-const Scoreboard = () => {
-	//asdasdasda
-	return <div></div>;
+const Scoreboard = ({ userName }) => {
+	const [results, setResults] = useState(0);
+	const [avatar, setAvatar] = useState('');
+	useEffect(() => {
+		const database = getDatabase(firebaseConfig);
+		const databaseRef = ref(database, 'users/' + userName);
+		onValue(databaseRef, (response) => {
+			const data = response.val().score;
+			const dataImg = response.val().avatar;
+			setResults(data);
+			setAvatar(dataImg);
+			console.log('RESPONSE data', response.val().avatar);
+		});
+	}, []);
+	return (
+		<div>
+			<div className='result-section'>
+				<p>
+					{userName} , You scored {results} out of 10
+				</p>
+				<img className='icon' src={avatar} alt='avatar image' />
+			</div>
+		</div>
+	);
 };
 
 export default Scoreboard;
-
-// changed name
