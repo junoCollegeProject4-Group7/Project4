3dcbaf3eb4d3b212fee9fe4cb8a969e03e378178
Sammy Lam <99058689+SamLamElectrician@users.noreply.github.com>
2022-11-13 20:46:20 -0500

    Revert "Scoreboard"

--- src/pages/Scoreboard.js
+++ src/pages/Scoreboard.js
@@ -1,32 +1,11 @@
 import React from 'react';
-import { getDatabase, ref, onValue } from 'firebase/database';
-import { useState, useEffect } from 'react';
-import firebaseConfig from '../firebase';
+import { getDatabase, ref, onValue, push } from 'firebase/database';
 
-const Scoreboard = ({ userName }) => {
-	const [results, setResults] = useState(0);
-	const [avatar, setAvatar] = useState('');
-	useEffect(() => {
-		const database = getDatabase(firebaseConfig);
-		const databaseRef = ref(database, 'users/' + userName);
-		onValue(databaseRef, (response) => {
-			const data = response.val().score;
-			const dataImg = response.val().avatar;
-			setResults(data);
-			setAvatar(dataImg);
-			console.log('RESPONSE data', response.val().avatar);
-		});
-	}, []);
-	return (
-		<div>
-			<div className='result-section'>
-				<p>
-					{userName} , You scored {results} out of 10
-				</p>
-				<img className='icon' src={avatar} alt='avatar image' />
-			</div>
-		</div>
-	);
+const Scoreboard = () => {
+	//asdasdasda
+	return <div></div>;
 };
 
 export default Scoreboard;
+
+// changed name
