49efc876eb8be4d104dd74a74a979da3fc5a5397
Sammy <sammylam_1@hotmail.com>
2022-11-16 23:56:19 -0500

    comments

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,139 +1,133 @@
 import { useState, useEffect } from 'react';
 import { getDatabase, ref, set, update } from 'firebase/database';
-// group components/imports
+import { v4 } from 'uuid';
+// components
 import Timer from './Timer';
 import Scoreboard from '../pages/Scoreboard';
-import { v4 } from 'uuid';
 
 //api is https://opentdb.com/api_config.php
 
 //example api https://opentdb.com/api.php?amount=10&category=25&difficulty=medium&type=multiple
 const QuestionCard = ({ question, userName, resetTimer, stopTimer, count }) => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [score, setScore] = useState(0);
-  const [timerReset, setTimerReset] = useState(false);
-  const [showScore, setShowScore] = useState(true);
-  const [answerBank, setAnswerBank] = useState([]);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [score, setScore] = useState(0);
 
-  const questions = question.map(function (q) {
-    return q.question;
-  });
+	const [showScore, setShowScore] = useState(true);
+	const [answerBank, setAnswerBank] = useState([]);
 
-  // shuffling the answers method https://bost.ocks.org/mike/shuffle/ curtesy of Paco
-  // create folder
-  function shuffle(ans) {
-    let m = ans.length,
-      t,
-      i;
-    // While there remain elements to shuffle…
-    if (m) {
-      // Pick a remaining element…
-      i = Math.floor(Math.random() * m--);
-      // And swap it with the current element.
-      t = ans[m];
-      ans[m] = ans[i];
-      ans[i] = t;
-    }
-    return ans;
-  }
-  useEffect(() => {
-    const ansArray = [];
-    question.forEach(element => {
-      const answer = `${element.incorrect_answers.toString()},${element.correct_answer}`.split(',')
-      ansArray.push(shuffle(answer))
-    });
-    setAnswerBank(ansArray)
-  }, [question])
+	//creates a question bank from the props
+	const questions = question.map(function (q) {
+		return q.question;
+	});
 
-  // }); //Shuffle array method
+	// shuffling the answers method https://bost.ocks.org/mike/shuffle/ curtesy of Paco
+	// create folder
+	function shuffle(ans) {
+		let m = ans.length,
+			t,
+			i;
+		// While there remain elements to shuffle…
+		if (m) {
+			// Pick a remaining element…
+			i = Math.floor(Math.random() * m--);
+			// And swap it with the current element.
+			t = ans[m];
+			ans[m] = ans[i];
+			ans[i] = t;
+		}
+		return ans;
+	}
+	//shuffle function
+	useEffect(() => {
+		const ansArray = [];
+		question.forEach((element) => {
+			const answer = `${element.incorrect_answers.toString()},${
+				element.correct_answer
+			}`.split(',');
+			ansArray.push(shuffle(answer));
+		});
+		setAnswerBank(ansArray);
+	}, [question]);
 
-  // const allAsnwers = [...ans.incorrect_answers, ans.correct_answer];
-  // const shuffledAns = shuffle(allAsnwers);
-  // console.log(shuffledAns, "these are all the shuffled Ans")
-  // console.log(currentQuestion, "this is answer.correct_answer")
-
-  // shuffle date b/f rendering 
-
-  // extra code ends here 
-
-  const correctAns = question.map(function (answer) {
-    return answer.correct_answer;
-  });
-
-  const updateFirebase = () => {
-    const db = getDatabase();
-    update(ref(db, `users/${userName}`), {
-      score: score,
-    });
-  };
-
-  useEffect(() => {
-    updateFirebase();
-  }, [score]);
-
-  const handleSubmit = (e) => {
-    setCurrentQuestion(currentQuestion + 1);
-    if (e.target.textContent === correctAns[currentQuestion]) {
-      setScore(score + 1);
-    } else {
-      setScore(score);
-    }
-
-    const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < questions.length) {
-      setCurrentQuestion(nextQuestion);
-      resetTimer();
-    } else {
-      setShowScore(false);
-      stopTimer();
-    }
-  };
-  console.log(answerBank)
-  return (
-    <>
-      {showScore ? (
-        <div className="card">
-          <Timer count={count} />
-          <Timer currentQuestion={currentQuestion} />
-          <div className="question">
-            {/* questions.length > 0 && */}
-            {questions.length > 0 &&
-              questions[currentQuestion]
-                .replace(/&quot;/g, '"')
-                .replace(/&rsquo;/g, "'")
-                .replace(/&Eacute;/g, 'é')
-                .replace(/&#039;/g, "'")
-                .replace(/&shy;/g, '')}
-          </div>
-          <div className="answers">
-            <div>
-              {/* splits answer array by delimiter and maps the array adding a button to handle  */}
-              {answerBank[currentQuestion] &&
-                answerBank[currentQuestion].map((ans) => {
-                  return (
-                    <button
-                      key={v4()}
-                      onClick={(e) => {
-                        handleSubmit(e);
-                      }}
-                    >
-                      {ans
-                        .replace(/&quot;/g, '"')
-                        .replace(/&rsquo;/g, "'")
-                        .replace(/&Eacute;/g, 'é')
-                        .replace(/&#039;/g, "'")
-                        .replace(/&shy;/g, '')}
-                    </button>
-                  );
-                })}
-            </div>
-          </div>
-        </div>
-      ) : (
-        <Scoreboard userName={userName} />
-      )}
-    </>
-  );
+	//data for correct answer to cross reference with clicked answer
+	const correctAns = question.map(function (answer) {
+		return answer.correct_answer;
+	});
+	//update score to firebase
+	const updateFirebase = () => {
+		const db = getDatabase();
+		update(ref(db, `users/${userName}`), {
+			score: score,
+		});
+	};
+	//run update score if score changes
+	useEffect(() => {
+		updateFirebase();
+	}, [score]);
+	//cross ref to correct answer to selected answers
+	const handleSubmit = (e) => {
+		setCurrentQuestion(currentQuestion + 1);
+		if (e.target.textContent === correctAns[currentQuestion]) {
+			setScore(score + 1);
+		} else {
+			setScore(score);
+		}
+		//counter to move question to the next question
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+			resetTimer();
+		} else {
+			setShowScore(false);
+			stopTimer();
+		}
+	};
+	//sets current question and changes regex
+	return (
+		<>
+			{showScore ? (
+				<div className='card'>
+					<Timer count={count} />
+					<Timer currentQuestion={currentQuestion} />
+					<div className='question'>
+						{/* questions.length > 0 && */}
+						{questions.length > 0 &&
+							questions[currentQuestion]
+								.replace(/&quot;/g, '"')
+								.replace(/&rsquo;/g, "'")
+								.replace(/&Eacute;/g, 'é')
+								.replace(/&#039;/g, "'")
+								.replace(/&shy;/g, '')}
+					</div>
+					<div className='answers'>
+						<div>
+							{/* splits answer array by delimiter and maps the array adding a button to handle  */}
+							{answerBank[currentQuestion] &&
+								answerBank[currentQuestion].map((ans) => {
+									return (
+										<button
+											key={v4()}
+											onClick={(e) => {
+												handleSubmit(e);
+											}}
+										>
+											{ans
+												.replace(/&quot;/g, '"')
+												.replace(/&rsquo;/g, "'")
+												.replace(/&Eacute;/g, 'é')
+												.replace(/&#039;/g, "'")
+												.replace(/&shy;/g, '')}
+										</button>
+									);
+								})}
+						</div>
+					</div>
+				</div>
+			) : (
+				<Scoreboard userName={userName} />
+			)}
+		</>
+	);
 };
 
 export default QuestionCard;
--- src/components/QuizSelector.js
+++ src/components/QuizSelector.js
@@ -9,24 +9,27 @@ import mascot from '../assests/meeseeks-removebg-preview.png';
 
 //example api https://opentdb.com/api.php?amount=10&category=25&difficulty=medium&type=multiple
 const QuizSelector = () => {
+	//sets the catergory chosen
 	const [category, setCategory] = useState(0);
+	//sets t
 	const [difficulty, setDifficulty] = useState('');
 	const [questionBank, setQuestionBank] = useState([]);
-	const [loading, setIsLoading] = useState(true);
-	const [avatarLoading, setAvatarLoading] = useState(true);
-	const [quizLoad, setQuizLoad] = useState(true);
 	const [avatar, setAvatar] = useState('');
 	const [userName, setUserName] = useState('');
+	//loading states
+	const [loading, setIsLoading] = useState(true);
+	const [avatarLoading, setAvatarLoading] = useState(true);
+	//unique id generator
 	const id = v4();
-
+	//states regarding timer
 	const answerTime = 30;
 	const [count, setCount] = useState(answerTime);
 	const [timer, setTimer] = useState(undefined);
-
+	//use effect to do timer
 	useEffect(() => {
 		if (count === 0) stopTimer();
 	}, [count]);
-
+	//function to start the timer from 30
 	const startTimer = () => {
 		setTimer(
 			setInterval(() => {
@@ -34,31 +37,30 @@ const QuizSelector = () => {
 			}, 1000)
 		);
 	};
-
-	// ternary logic for answer feedback 
+	//resets timer on each question
 
 	const resetTimer = () => {
 		clearInterval(timer);
 		setCount(answerTime);
 		startTimer();
 	};
-
+	//stops the timer on finish
 	const stopTimer = () => {
 		clearInterval(timer);
 	};
-
+	//click function to handle avatar search and set the loading states
 	const handleSearchAvatar = (e) => {
 		e.preventDefault();
 		avatarGen();
 		setAvatarLoading(false);
 	};
-
+	//error handling to make sure user selects both avatar and username
 	useEffect(() => {
 		if (avatar && userName) {
 			pushFirebase();
 		}
 	}, [avatar]);
-
+	//push avatar data to firebase
 	const pushFirebase = () => {
 		const db = getDatabase();
 		set(ref(db, 'users/' + userName), {
@@ -67,7 +69,7 @@ const QuizSelector = () => {
 			score: 0,
 		});
 	};
-
+	//axios call to api
 	const avatarGen = async () => {
 		try {
 			const response = await axios
@@ -81,7 +83,7 @@ const QuizSelector = () => {
 			alert(error);
 		}
 	};
-
+	//axios call to trivia api only if all requirements fufilled
 	const questions = async () => {
 		try {
 			if (difficulty && category && userName && avatar) {
@@ -96,7 +98,7 @@ const QuizSelector = () => {
 			alert(err);
 		}
 	};
-
+	//handles the onclick page for the question generator
 	const handleSubmitQuestion = (e) => {
 		e.preventDefault();
 		questions();
@@ -104,15 +106,17 @@ const QuizSelector = () => {
 		startTimer();
 	};
 
-	//true and true and get this
+	//true and true and get the avatar selection page
 	if (avatarLoading && loading) {
 		return (
 			<form onSubmit={(e) => handleSearchAvatar(e)} className='charGen'>
 				<div className='welcomeAvatar'>
-					<img className="logoAvatar-Hello" src={mascot} alt="Trvia Time Avatar" />
-					<h2 className='speechBubble'>
-						Nice to meet you! What's your name?
-					</h2>
+					<img
+						className='logoAvatar-Hello'
+						src={mascot}
+						alt='Trvia Time Avatar'
+					/>
+					<h2 className='speechBubble'>Nice to meet you! What's your name?</h2>
 				</div>
 				<label htmlFor='Character Icon Generator'></label>
 				<input
@@ -121,15 +125,21 @@ const QuizSelector = () => {
 					// sets username
 					value={userName}
 					onChange={(e) => setUserName(e.target.value)}
+					required
 				/>
 			</form>
 		);
+		//true and false, once avatar is selected then quiz generator page shows
 	} else if (loading && !avatarLoading) {
 		return (
 			<>
 				<div className='userCreate'>
 					<h2 className='userName'>Hello, {userName}!</h2>
-					<img className='userAvatar' src={avatar} alt='API Generated Avatar'></img>
+					<img
+						className='userAvatar'
+						src={avatar}
+						alt='API Generated Avatar'
+					></img>
 					<form onSubmit={(e) => handleSubmitQuestion(e)}>
 						<label htmlFor='quizCategory'></label>
 						<select
@@ -145,7 +155,7 @@ const QuizSelector = () => {
 							<option value={14}>Television</option>
 							<option value={16}>Board Games</option>
 							<option value={17}>Science & Nature</option>
-							<option value={18}>Computers</option>    {/* is not working */}
+							<option value={18}>Computers</option> {/* is not working */}
 							<option value={20}>Mythology</option>
 							<option value={21}>Sports</option>
 							<option value={22}>Geography</option>
@@ -175,9 +185,11 @@ const QuizSelector = () => {
 				</div>
 			</>
 		);
+		//once avatar and quiz params selected, go to questionCard
 	} else if (!avatarLoading && !loading) {
 		return (
 			<>
+				{/* passes down api data and timer data */}
 				<QuestionCard
 					question={questionBank}
 					userName={userName}
